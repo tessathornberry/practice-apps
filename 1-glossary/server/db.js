bfdc2f6392c3f77e7001e60a8e16d104c3f0b0d1
Tessa Thornberry <tthornberryclass@gmail.com>
2022-10-05 12:14:52 -0700

    attempting to set up database with mongoDB/mongoose

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,5 +1,43 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const dbName = 'glossary';
+// let db;
+// const url = "mongodb://127.0.0.1:27017/glossary";
+const url = "http://localhost:3000/glossary";
 
+
+mongoose.connect(url, { useNewUrlParser: true});
+
+let db = mongoose.connection;
+db.once('open', _ => {
+  console.log('Database connected: ', url);
+});
+db.on('error', err => {
+  console.log('Connection error: ', url);
+});
+// db = client.db(dbName);
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// console.log(`Connected MongoDB: ${url}`);
+// console.log(`Database/: ${dbName}`);
+
+
+// const glossarySchema = new Schema({
+//   word: String,
+//   definition: String
+// });
+
+// const Glossary = mongoose.model('Glossary', glossarySchema);
+
+// const wordInstance = new Glossary({word:'defenestration', definition: 'to throw someone or something out of a window'});
+
+// wordInstance.save((err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+
+// })
+
+module.exports = db;
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -4,23 +4,23 @@ const express = require("express");
 const path = require("path");
 const cors = require('cors');
 const morgan = require('morgan');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
+//logging and parsing - I've always seen these
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
-/****
- *
- *
- * Other routes here....
- *
- *
- */
-const port = process.env.PORT || 3000;
-// app.set('port', process.env.PORT || 3000);
+
+// methods sent by client
+app.get();
+app.post();
+app.put();
+app.delete();
+
 app.listen(port);
 console.log(`Listening at http://localhost:${port}`);
