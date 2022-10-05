5797cc48d1feadcaf7265eeefefa9981952f7276
Tessa Thornberry <tthornberryclass@gmail.com>
2022-10-05 10:26:27 -0700

    updated server index.js to include dependencies and server now listening on 3000

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,26 @@
 require("dotenv").config();
+const db = require('./db.js')
 const express = require("express");
 const path = require("path");
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
-
-app.listen(process.env.PORT);
-console.log(`Listening at http://localhost:${process.env.PORT}`);
+const port = process.env.PORT || 3000;
+// app.set('port', process.env.PORT || 3000);
+app.listen(port);
+console.log(`Listening at http://localhost:${port}`);
