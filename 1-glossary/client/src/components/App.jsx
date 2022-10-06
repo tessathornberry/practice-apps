3cedddf8beb30e5fe245cac632b03d7a8aff62f2
Tessa Thornberry <tthornberryclass@gmail.com>
2022-10-06 11:32:04 -0700

    add new word and definition to DOM

--- 1-glossary/client/src/components/App.jsx
+++ 1-glossary/client/src/components/App.jsx
@@ -13,51 +13,78 @@ useEffect((input) => {
   axios.get('http://localhost:3000/glossary', input)
   .then((response) => {
     for (var i = 0; i < response.data.length; i++) {
-      var currentWord = response.data[i];
-      var obj = {}; //not sure I need all this
-      obj.word = currentWord.word;
-      obj.definition = currentWord.definition;
-      obj.id = currentWord._id;
-      wordHolder.push(obj);
+      wordHolder.push(response.data[i]);
     }
-    console.log('wordHolder', wordHolder);
     setWords(wordHolder);
-    // console.log('response', response.data)
   })
   .catch(err => console.log(err));
 }, []);
+
+var addWord = (entry) => { //has to be in object format
+  axios.post('http://localhost:3000/glossary', entry)
+    .then((response) => {
+      //if post is successful, re-render page with obj without id, or re-get?
+      console.log('response data', response.data);
+      setWords([...wordHolder, entry]);
+    })
+    .catch(err => console.log(err));
+}
 
  //I need to map the words and send them to components
   return (
   <div>
     <h2>Glossary</h2>
+    <AddWordForm handleSubmission={addWord}/>
     <div><form></form></div>
     <WordList words={words}/>
-
-
-
   </div>
   );
 };
 /******************* WordList ******************/
 const WordList = ({words}) => {
-  console.log('words in WordList', words);
   return (
     <ul>
       {words.map((word) =>
-        <WordEntry word={word.word} definition={word.definition} key={word.id}/>
+        <WordEntry word={word.word} definition={word.definition} key={word._id}/>
       )}
     </ul>
   )
 };
 
-
 /******************* WordEntry ******************/
 const WordEntry = ({word, definition}) => {
   console.log('word', word);
   console.log('definition', definition);
+
   return (
-    <li style={{"list-style-type":"none"}}>{word}: {definition}</li>
+    <li style={{"fontSize":"large","listStyleType":"none"}}>
+         {word}:    {definition}
+      <div>
+      <button>edit</button>
+      <button>delete</button>
+      </div>
+    </li>
+
+  )
+};
+
+const AddWordForm = ({handleSubmission}) => {
+  const [newWordEntry, setNewWordEntry] = useState('');
+  const [newDefEntry, setNewDefEntry] = useState('');
+
+
+  return (
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      var newWord = {};
+      newWord.word = newWordEntry;
+      newWord.definition = newDefEntry;
+      handleSubmission(newWord)
+    }}>
+      <input type="text" placeholder="Add a new word..." value={newWordEntry} onChange={(event) => setNewWordEntry(event.target.value)}/>
+      <input type="text" placeholder="Add a definition..." value={newDefEntry} onChange={(event) => setNewDefEntry(event.target.value)}/>
+      <button type="submit">Add new word</button>
+    </form>
   )
 }
 
