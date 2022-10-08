1e7aaac122d4bb1b4a6d42dbc56d52683b39f065
Tessa Thornberry <tthornberryclass@gmail.com>
2022-10-08 14:42:06 -0700

    added app and form visibility to client side

---
+++ 2-checkout/client/src/components/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+
+const {useState, useEffect} = React;
+
+const App = () => {
+  const [page, setPage] = useState(1);
+  const [isVisible, setIsVisible] = useState(false);
+  const [F1Visible, setF1Visible] = useState(false);
+  const [F2Visible, setF2Visible] = useState(true);
+  const [F3Visible, setF3Visible] = useState(false);
+
+  /** declare result object and assign cookie as a value */
+  var checkoutResponse = {};
+  checkoutResponse.s_id = document.cookie;
+
+  /** function to set forms visible or not */
+  var Form = ({formNumber, isVisible}) => {
+    if (isVisible) {
+      return <div className="form">{formNumber}, this one</div>;
+    }
+    return null;
+
+    // return <div className="form">{formNumber}, make null</div>;
+  }
+
+  console.log(checkoutResponse);
+  //I want my checkout button to live here
+  if (page === 1) {
+   console.log("hooray, page 1!")
+  } else if (page === 2) {
+    console.log("hooray, page 2!")
+  } else if (page === 3) {
+    console.log("hooray, page 3!")
+  } else {
+    console.log("should have checked out already!");
+  }
+
+  var openCart = (event) => {
+    event.preventDefault();
+      setPage(page + 1);
+
+  }
+
+  return (
+    <div>
+      <h3>Hi hi!</h3>
+      <Form isVisible={F1Visible} formNumber="F1" />
+      <Form isVisible={F2Visible} formNumber="F2" />
+      <Form isVisible={F3Visible} formNumber="F3" />
+      <button id="checkout" onClick={(event) => {openCart(event)}}>Checkout</button>
+    </div>
+
+  )
+};
+
+export default App;
--- 2-checkout/client/src/index.jsx
+++ 2-checkout/client/src/index.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import App from ('./App.jsx')
-import { render } from "react-dom";
+import App from './components/App.jsx';
+// import { render } from "react-dom";
+import ReactDOM from 'react-dom';
 
-render(
+
+ReactDOM.render(
   <div>
     <p>Hello, World!</p>
-    <p><App/>
+    <p>
       <code>Page Cookie: {JSON.stringify(document.cookie, undefined, "\t")}</code>
     </p>
+    <App/>
   </div>,
   document.getElementById("root")
 );
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+*/
+// ReactDOM.render(<App />, document.getElementById("root"));
