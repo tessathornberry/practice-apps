c9e8fc6ee4d0d6d67a380bb671469a9f5b3ba461
Tessa Thornberry <tthornberryclass@gmail.com>
2022-10-08 14:48:50 -0700

    button alternatively renders form representations

--- 2-checkout/client/src/components/App.jsx
+++ 2-checkout/client/src/components/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const [page, setPage] = useState(1);
   const [isVisible, setIsVisible] = useState(false);
   const [F1Visible, setF1Visible] = useState(false);
-  const [F2Visible, setF2Visible] = useState(true);
+  const [F2Visible, setF2Visible] = useState(false);
   const [F3Visible, setF3Visible] = useState(false);
 
   /** declare result object and assign cookie as a value */
@@ -20,26 +20,43 @@ const App = () => {
       return <div className="form">{formNumber}, this one</div>;
     }
     return null;
-
     // return <div className="form">{formNumber}, make null</div>;
   }
 
   console.log(checkoutResponse);
   //I want my checkout button to live here
-  if (page === 1) {
-   console.log("hooray, page 1!")
-  } else if (page === 2) {
-    console.log("hooray, page 2!")
-  } else if (page === 3) {
-    console.log("hooray, page 3!")
-  } else {
-    console.log("should have checked out already!");
-  }
+  // if (page === 1) {
+  //  console.log("hooray, page 1!")
+  // } else if (page === 2) {
+  //   setF1Visible(true);
+  //   console.log("hooray, page 2!")
+  // } else if (page === 3) {
+  //   setF1Visible(false);
+  //   setF2Visible(true);
+  //   console.log("hooray, page 3!")
+  // } else {
+  //   setF2Visible(false);
+  //   setF3Visible(true);
+  //   console.log("should have checked out already!");
+  // }
 
   var openCart = (event) => {
     event.preventDefault();
       setPage(page + 1);
-
+      if (page === 1) {
+        console.log("hooray, page 1!")
+       } else if (page === 2) {
+         setF1Visible(true);
+         console.log("hooray, page 2!")
+       } else if (page === 3) {
+         setF1Visible(false);
+         setF2Visible(true);
+         console.log("hooray, page 3!")
+       } else {
+         setF2Visible(false);
+         setF3Visible(true);
+         console.log("should have checked out already!");
+       }
   }
 
   return (
