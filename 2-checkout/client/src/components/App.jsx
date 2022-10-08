e02747a42f78e2318477b597f7f062e31f99f850
Tessa Thornberry <tthornberryclass@gmail.com>
2022-10-08 14:55:29 -0700

    refactored form visibility function to ? : conditional

--- 2-checkout/client/src/components/App.jsx
+++ 2-checkout/client/src/components/App.jsx
@@ -5,7 +5,7 @@ const {useState, useEffect} = React;
 
 const App = () => {
   const [page, setPage] = useState(1);
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
   const [F1Visible, setF1Visible] = useState(false);
   const [F2Visible, setF2Visible] = useState(false);
   const [F3Visible, setF3Visible] = useState(false);
@@ -16,47 +16,33 @@ const App = () => {
 
   /** function to set forms visible or not */
   var Form = ({formNumber, isVisible}) => {
-    if (isVisible) {
-      return <div className="form">{formNumber}, this one</div>;
-    }
-    return null;
+    return isVisible ? <div className="form">{formNumber}, this one</div> : null;
+
+    // if (isVisible) {
+    //   return <div className="form">{formNumber}, this one</div>;
+    // }
+    // return null;
     // return <div className="form">{formNumber}, make null</div>;
   }
 
-  console.log(checkoutResponse);
-  //I want my checkout button to live here
-  // if (page === 1) {
-  //  console.log("hooray, page 1!")
-  // } else if (page === 2) {
-  //   setF1Visible(true);
-  //   console.log("hooray, page 2!")
-  // } else if (page === 3) {
-  //   setF1Visible(false);
-  //   setF2Visible(true);
-  //   console.log("hooray, page 3!")
-  // } else {
-  //   setF2Visible(false);
-  //   setF3Visible(true);
-  //   console.log("should have checked out already!");
-  // }
-
   var openCart = (event) => {
     event.preventDefault();
-      setPage(page + 1);
-      if (page === 1) {
-        console.log("hooray, page 1!")
-       } else if (page === 2) {
-         setF1Visible(true);
-         console.log("hooray, page 2!")
-       } else if (page === 3) {
-         setF1Visible(false);
-         setF2Visible(true);
-         console.log("hooray, page 3!")
-       } else {
-         setF2Visible(false);
-         setF3Visible(true);
-         console.log("should have checked out already!");
-       }
+    setPage(page + 1);
+    if (page === 1) {
+      console.log("hooray, page 1!")
+      setF1Visible(true);
+    } else if (page === 2) {
+      console.log("hooray, page 2!")
+      setF1Visible(false);
+      setF2Visible(true);
+    } else if (page === 3) {
+      console.log("hooray, page 3!")
+      setF2Visible(false);
+      setF3Visible(true);
+    } else {
+      console.log("should have checked out already!");
+      setF3Visible(false);
+    }
   }
 
   return (
