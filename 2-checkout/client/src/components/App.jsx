e9a79315dbc8cb4364d8edf55fb4f1c2601a77b7
Tessa Thornberry <tthornberryclass@gmail.com>
2022-10-08 15:49:52 -0700

    made fields required for user moving forward`:wq

--- 2-checkout/client/src/components/App.jsx
+++ 2-checkout/client/src/components/App.jsx
@@ -84,9 +84,9 @@ const Form1 = ({object}) => {
       console.log('object in Form 1', object);
 
     }}>
-      <div><input type="text" placeholder="username..." value={name} onChange={(event) => setName(event.target.value)}></input></div>
-      <div><input type="text" placeholder="e-mail..." value={email} onChange={(event) => setEmail(event.target.value)}></input></div>
-      <div><input type="text" placeholder="password..." value={password} onChange={(event) => setPassword(event.target.value)}></input></div>
+      <div><input type="text" placeholder="username..." value={name} onChange={(event) => setName(event.target.value)} required></input></div>
+      <div><input type="text" placeholder="e-mail..." value={email} onChange={(event) => setEmail(event.target.value)} required></input></div>
+      <div><input type="text" placeholder="password..." value={password} onChange={(event) => setPassword(event.target.value)} required></input></div>
       <div><button type="submit">Next</button></div>
 
 
